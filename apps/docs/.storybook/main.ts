3b3dbcbbd1c29a46659e85354fe3ef8331817104
Siddhesh Tawde <siddheshtawde35@gmail.com>
2024-12-07 13:13:58 +0530

    Changes to storybook

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,39 +1,28 @@
 import { join, dirname } from "path";
-import type { StorybookConfig } from "@storybook/nextjs";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string) {
+function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, "package.json")));
 }
 
-const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
   addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@chromatic-com/storybook"),
     getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-mdx-gfm"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-mdx-gfm"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-mdx-gfm"),
-    "@chromatic-com/storybook"
   ],
-
   framework: {
     name: getAbsolutePath("@storybook/nextjs"),
     options: {},
-  },
-
-  docs: {},
-
-  typescript: {
-    reactDocgen: "react-docgen-typescript",
   },
 };
 export default config;
